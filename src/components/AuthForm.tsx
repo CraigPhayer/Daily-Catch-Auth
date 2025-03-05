7d94327836f56211180899071950936a7ffd57b9
Craig_ <21340633@studentmail.ul.ie>
2025-03-05 13:12:27 +0000

    AuthForm.tsx useState<string | null>(null) allows both null and string

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Image from "next/image";
 import InputField from "@/components/InputFields";
 
@@ -14,34 +14,47 @@ interface AuthFormProps {
     linkQuestion: string;
 }
 
-export default function AuthForm({ title, subtitle, buttonText, onSubmit, linkText, linkHref, linkQuestion }: AuthFormProps) {
+export default function AuthForm({
+                                     title,
+                                     subtitle,
+                                     buttonText,
+                                     onSubmit,
+                                     linkText,
+                                     linkHref,
+                                     linkQuestion
+                                 }: AuthFormProps) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+    const [error, setError] = useState<string | null>(null);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
             onSubmit(email, password);
-        } catch (error: any) {
-            setError(error.message);
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
         }
     };
 
     return (
         <div className="w-full md:w-1/2 flex flex-col justify-center items-center px-8 bg-white">
             <div className="w-full max-w-md flex flex-col items-center">
-                <Image src="/logo2.svg" alt="Auth Icon" width={350} height={350} />
+                <Image src="/logo2.svg" alt="Auth Icon" width={350} height={350}/>
                 <h2 className="text-2xl font-bold mt-4 text-primary">{title}</h2>
                 <p className="text-primary mb-6">{subtitle}</p>
 
                 {error && <p className="text-red-500 mb-2">{error}</p>}
 
                 <form onSubmit={handleSubmit} className="w-full max-w-sm">
-                    <InputField type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} iconSrc="/email.svg" />
-                    <InputField type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} iconSrc="/password.svg" />
+                    <InputField type="email" placeholder="Email" value={email}
+                                onChange={(e) => setEmail(e.target.value)} iconSrc="/email.svg"/>
+                    <InputField type="password" placeholder="Password" value={password}
+                                onChange={(e) => setPassword(e.target.value)} iconSrc="/password.svg"/>
 
-                    <button type="submit" className="w-full bg-primary text-white p-3 rounded-lg hover:bg-secondary transition">
+                    <button type="submit"
+                            className="w-full bg-primary text-white p-3 rounded-lg hover:bg-secondary transition">
                         {buttonText}
                     </button>
                 </form>
