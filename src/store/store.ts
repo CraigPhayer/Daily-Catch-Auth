c9f5a6de7479d0a4237a7f2b98ec7d8bfcdd81ba
Craig_ <21340633@studentmail.ul.ie>
2025-03-12 13:22:59 +0000

    Added redux-persist to keep logged in state on refresh for dashboard

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,15 @@
 
 import {configureStore} from "@reduxjs/toolkit";
 import {authReducer} from "./authSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+    key: 'auth', // key for local storage
+    storage, //local storage
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer); // Wrap authReducer
 
 export const store = configureStore({
     reducer: {
@@ -9,5 +18,6 @@ export const store = configureStore({
     },
 });
 
+export const persistor = persistStore(store); // for Redux store
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
